480754f01f6f1c94214b09a250984afaa30e3fae
Mohan K <mohank@tests-iMac.Dlink>
2023-04-26 19:17:52 +0530

    first commit

---
+++ MY PROJECT/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  MY PROJECT
+//
+//  Created by Mohan K on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameone: UILabel!
+    
+    
+    @IBOutlet weak var last: UITextField!
+    
+    
+    
+    @IBOutlet weak var FirstTextField: UITextField!
+   
+    @IBOutlet weak var RegisterLAbel: UILabel!
+
+    @IBOutlet weak var nametwo: UILabel!
+    
+   
+   
+    @IBOutlet weak var mobile: UITextField!
+    
+    @IBOutlet weak var mailid: UITextField!
+    
+    @IBOutlet weak var mail: UILabel!
+    
+    
+    @IBOutlet weak var number: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
